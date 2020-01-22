6cc87783f35febc322920ab486383fd4a5324d33
MrMaverick-Dev <alex.ray.dupont@gmail.com>
2020-01-22 22:17:00 +0100

    old-version

--- main.js
+++ main.js
@@ -1,194 +1,194 @@
-let cases = [
-  Array.from(document.getElementsByClassName("row-1")),
-  Array.from(document.getElementsByClassName("row-2")),
-  Array.from(document.getElementsByClassName("row-3"))
-];
-
+let cases = Array.from(document.getElementsByClassName("case"));
 let p1Score = document.getElementById("p1-points");
 let p2Score = document.getElementById("p2-points");
-let displayTurnPlayerName = document.getElementById("turn-player-name");
-const newGameButton = document.getElementById("new-game");
-const resetButton = document.getElementById("reset");
+let newGameButton = document.getElementById("new-game");
+let resetButton = document.getElementById("reset");
+let turnPlayer = document.getElementById("turn-player");
+let turnDisplay = document.getElementById("turn-display")
+let turnSentence = document.getElementById("turn-sentence-begin")
+let turnSentenceEnd = document.getElementById("turn-sentence-end")
 
-let playerTurn = 0;
+
+let playerOne = 1;
+let playerTwo = 2;
 let playerOneScore = 0;
 let playerTwoScore = 0;
-const playerOneId = "X";
-const playerTwoId = "O";
+let turn = 0;
 let winner = false;
 
-let gameState = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
-
-const ERows = (ECols = Object.freeze({
-  FIRST: 0,
-  SECOND: 1,
-  THIRD: 2
-}));
-
-function checkingRows() {
-  if (
-    gameState[ERows.FIRST][ECols.FIRST] === gameState[ERows.FIRST][ECols.SECOND] &&
-    gameState[ERows.FIRST][ECols.FIRST] === gameState[ERows.FIRST][ECols.THIRD] &&
-    gameState[ERows.FIRST][ECols.FIRST] != null) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.FIRST][ECols.FIRST], cases[ERows.FIRST][ECols.SECOND], cases[ERows.FIRST][ECols.THIRD]);
-  } else if (
-    gameState[ERows.SECOND][ECols.FIRST] === gameState[ERows.SECOND][ECols.SECOND] &&
-    gameState[ERows.SECOND][ECols.FIRST] === gameState[ERows.SECOND][ECols.THIRD] &&
-    gameState[ERows.SECOND][ECols.FIRST] != null) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.SECOND][ECols.FIRST], cases[ERows.SECOND][ECols.SECOND], cases[ERows.SECOND][ECols.THIRD]);
-  } else if (
-    gameState[ERows.THIRD][ECols.FIRST] === gameState[ERows.THIRD][ECols.SECOND] &&
-    gameState[ERows.THIRD][ECols.SECOND] === gameState[ERows.THIRD][ECols.THIRD] &&
-    gameState[ERows.THIRD][ECols.FIRST] != null) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.THIRD][ECols.FIRST], cases[ERows.THIRD][ECols.SECOND], cases[ERows.THIRD][ECols.THIRD]);
-  }
-}
-
-function checkingColumn() {
-  if (
-    gameState[ERows.FIRST][ECols.FIRST] === gameState[ERows.SECOND][ECols.FIRST] &&
-    gameState[ERows.FIRST][ECols.FIRST] === gameState[ERows.THIRD][ECols.FIRST] &&
-    gameState[ERows.FIRST][ECols.FIRST] != null) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.FIRST][ECols.FIRST], cases[ERows.SECOND][ECols.FIRST], cases[ERows.THIRD][ECols.FIRST]);
-  } else if (
-    gameState[ERows.FIRST][ECols.SECOND] === gameState[ERows.SECOND][ECols.SECOND] &&
-    gameState[ERows.FIRST][ECols.SECOND] === gameState[ERows.THIRD][ECols.SECOND] &&
-    gameState[ERows.FIRST][ECols.SECOND] != null) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.FIRST][ECols.SECOND], cases[ERows.SECOND][ECols.SECOND], cases[ERows.THIRD][ECols.SECOND]);
-  } else if (
-    gameState[ERows.FIRST][ECols.THIRD] === gameState[ERows.SECOND][ECols.THIRD] &&
-    gameState[ERows.FIRST][ECols.THIRD] === gameState[ERows.THIRD][ECols.THIRD] &&
-    gameState[ERows.FIRST][ECols.THIRD] != null) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.FIRST][ECols.THIRD], cases[ERows.SECOND][ECols.THIRD], cases[ERows.THIRD][ECols.THIRD]);
-  }
-}
-
-function checkingDiagonal() {
-  if (gameState[ERows.FIRST][ECols.FIRST] === gameState[ERows.SECOND][ECols.SECOND] &&
-    gameState[ERows.FIRST][ECols.FIRST] === gameState[ERows.THIRD][ECols.THIRD] &&
-    gameState[ERows.FIRST][ECols.FIRST] != null
-  ) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.FIRST][ECols.FIRST], cases[ERows.SECOND][ECols.SECOND], cases[ERows.THIRD][ECols.THIRD]);
-  } else if (
-    gameState[ERows.THIRD][ECols.FIRST] === gameState[ERows.SECOND][ECols.SECOND] &&
-    gameState[ERows.THIRD][ECols.FIRST] === gameState[ERows.FIRST][ECols.THIRD] &&
-    gameState[ERows.THIRD][ECols.FIRST] != null) {
-    displayWinnerPlayer(playerTurn);
-    colorWinningCases(cases[ERows.THIRD][ECols.FIRST], cases[ERows.SECOND][ECols.SECOND], cases[ERows.FIRST][ECols.THIRD]);
-  }
-}
-
-function colorWinningCases(caseOne, caseTwo, caseThree) {
-  caseOne.style.backgroundColor = "#26ff00";
-  caseTwo.style.backgroundColor = "#26ff00";
-  caseThree.style.backgroundColor = "#26ff00";
-}
-
 function checkingWinner() {
-  checkingRows();
-  checkingColumn();
-  checkingDiagonal();
+  if
+  //vérification Horizontale en partant de la première ligne
+  (
+    ((cases[0].textContent === cases[1].textContent) && (cases[0].textContent === cases[2].textContent)) &&
+    ((cases[0].textContent == "X") || cases[0].textContent == "O")
+  ) {
+    cases[0].style.backgroundColor = "#26ff00"
+    cases[1].style.backgroundColor = "#26ff00"
+    cases[2].style.backgroundColor = "#26ff00"
+    winner = true;
+  } else if (
+    ((cases[3].textContent === cases[4].textContent) && (cases[3].textContent === cases[5].textContent)) &&
+    ((cases[3].textContent == "X") || cases[3].textContent == "O")
+  ) {
+    cases[3].style.backgroundColor = "#26ff00"
+    cases[4].style.backgroundColor = "#26ff00"
+    cases[5].style.backgroundColor = "#26ff00"
+    winner = true;
+  } else if (
+    ((cases[6].textContent === cases[7].textContent) && (cases[6].textContent === cases[8].textContent)) &&
+    ((cases[6].textContent == "X") || cases[6].textContent == "O")
+  ) {
+    cases[6].style.backgroundColor = "#26ff00"
+    cases[7].style.backgroundColor = "#26ff00"
+    cases[8].style.backgroundColor = "#26ff00"
+    winner = true;
+  }
+  // Vérification Verticale en partant de la première colonne
+  else if (
+    ((cases[0].textContent === cases[3].textContent) && (cases[0].textContent === cases[6].textContent)) &&
+    ((cases[0].textContent == "X") || cases[0].textContent == "O")
+  ) {
+    cases[0].style.backgroundColor = "#26ff00"
+    cases[3].style.backgroundColor = "#26ff00"
+    cases[6].style.backgroundColor = "#26ff00"
+    winner = true;
+  } else if (
+    ((cases[1].textContent === cases[4].textContent) && (cases[1].textContent === cases[7].textContent)) &&
+    ((cases[1].textContent == "X") || cases[1].textContent == "O")
+  ) {
+    cases[1].style.backgroundColor = "#26ff00"
+    cases[4].style.backgroundColor = "#26ff00"
+    cases[7].style.backgroundColor = "#26ff00"
+    winner = true;
+  } else if (
+    ((cases[2].textContent === cases[5].textContent) && (cases[2].textContent === cases[8].textContent)) &&
+    ((cases[2].textContent == "X") || cases[2].textContent == "O")
+  ) {
+    cases[2].style.backgroundColor = "#26ff00"
+    cases[5].style.backgroundColor = "#26ff00"
+    cases[8].style.backgroundColor = "#26ff00"
+    winner = true;
+  }
+  // Vérification des Diagonales en partant du haut par la gauche 
+  else if (
+    ((cases[0].textContent === cases[4].textContent) && (cases[0].textContent === cases[8].textContent)) &&
+    ((cases[0].textContent == "X") || cases[0].textContent == "O")
+  ) {
+    cases[0].style.backgroundColor = "#26ff00"
+    cases[4].style.backgroundColor = "#26ff00"
+    cases[8].style.backgroundColor = "#26ff00"
+    winner = true;
+  } else if (
+    ((cases[2].textContent === cases[4].textContent) && (cases[2].textContent === cases[6].textContent)) &&
+    ((cases[2].textContent == "X") || cases[2].textContent == "O")
+  ) {
+    cases[2].style.backgroundColor = "#26ff00"
+    cases[4].style.backgroundColor = "#26ff00"
+    cases[6].style.backgroundColor = "#26ff00"
+    winner = true;
+  } else {}
 }
 
-function randomPlayerMatchStart() {
-  let number = Math.floor(Math.random() * 2) + 1;
-  if (number === 1) {
-    playerTurn = playerOneId;
+function randomStart() {
+  let number = Math.floor(Math.random() * 2) + 1
+  if (number === playerOne) {
+    turn = playerOne
+    turnSentence.textContent = "It's up to Player X to play"
   } else {
-    playerTurn = playerTwoId;
+    turn = playerTwo
+    turnSentence.textContent = "It's up to Player O to play"
   }
-  displayPlayerturn(playerTurn);
+
 }
 
-function switchPlayerTurn() {
-  if (playerTurn === playerOneId) {
-    playerTurn = playerTwoId;
+function togglePlayer() {
+  if (turn === 2) {
+    turnSentence.textContent = "It's up to Player X to play"
   } else {
-    playerTurn = playerOneId;
-  }
-}
-
-function displayPlayerturn(playerTurn) {
-  if (playerTurn === playerOneId) {
-    displayTurnPlayerName.textContent = "Player X";
-  } else {
-    displayTurnPlayerName.textContent = "Player O";
+    turnSentence.textContent = "It's up to Player O to play"
   }
 }
 
-function displayWinnerPlayer(playerTurn) {
-  if (playerTurn === playerOneId) {
-    playerOneScore += 1;
-    p1Score.textContent = playerOneScore;
+function aiChoice() {
+  let x = Math.floor(Math.random() * 9)
+  if (cases[x].textContent === "") {
+    cases[x].textContent = "O"
   } else {
-    playerTwoScore += 1;
-    p2Score.textContent = playerTwoScore;
+    aiChoice()
+    checkingWinner()
   }
-  winner = true;
 }
 
 function gameOne() {
   for (let i = 0; i < cases.length; i++) {
-    for (let x = 0; x < cases[i].length; x++) {
-      cases[i][x].addEventListener("click", function () {
-        if (!winner && (gameState[i][x] !== "X" && gameState[i][x] !== "O")) {
-          cases[i][x].textContent = playerTurn;
-          gameState[i][x] = playerTurn;
-          checkingWinner();
-          switchPlayerTurn();
-          displayPlayerturn(playerTurn);
+    cases[i].addEventListener("click", function () {
+      togglePlayer()
+      if ((turn === 1) && (cases[i].textContent === "") && (!winner)) {
+        cases[i].textContent = "X"
+        checkingWinner()
+        if (winner) {
+          playerOneScore++
+          p1Score.textContent = playerOneScore
+          turnSentence.textContent = "Player X win this match !"
+        } else {
+          turn++
         }
-      });
-    }
+      } else if ((turn === 2) && (cases[i].textContent === "") && (!winner)) {
+        cases[i].textContent = "O"
+        checkingWinner()
+        if (winner) {
+          playerTwoScore++
+          p2Score.textContent = playerTwoScore
+          turnSentence.textContent = "Player O win this match !"
+        } else {
+          turn--
+        }
+      }
+    })
   }
 }
 
-function newGame() {
-  newGameButton.addEventListener("click", function () {
-    for (let i = 0; i < cases.length; i++) {
-      for (let x = 0; x < cases[i].length; x++) {
-        cases[i][x].textContent = "";
-        cases[i][x].style.backgroundColor = "";
+function gameTwo() {
+  for (let i = 0; i < cases.length; i++) {
+    cases[i].addEventListener("click", function () {
+      if ((cases[i].textContent === "") && (!winner)) {
+        cases[i].textContent = "X"
+        checkingWinner()
+        if (winner) {
+          playerOneScore++
+          p1Score.textContent = playerOneScore
+        } else {
+          aiChoice()
+        }
       }
-    }
-    gameState = [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ];
-    winner = false;
-    playerTurn = 0;
-    randomPlayerMatchStart();
-    gameOne();
-  });
+    })
+  }
 }
 
-function reset() {
-  resetButton.addEventListener("click", function () {
-    playerOneScore = 0;
-    playerTwoScore = 0;
-    p1Score.textContent = playerOneScore;
-    p2Score.textContent = playerTwoScore;
-    newGame();
-  });
-}
+newGameButton.addEventListener("click", function () {
+  for (let i = 0; i < cases.length; i++) {
+    cases[i].textContent = ""
+    cases[i].style.backgroundColor = ""
+  }
+  winner = false;
+  randomStart();
+})
 
-function initApp() {
-  newGame();
-  reset();
-  randomPlayerMatchStart();
-  gameOne();
-}
+resetButton.addEventListener("click", function () {
+  for (let i = 0; i < cases.length; i++) {
+    cases[i].textContent = ""
+    cases[i].style.backgroundColor = ""
+  }
+  winner = false;
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  p1Score.textContent = playerOneScore
+  p2Score.textContent = playerTwoScore
+  randomStart();
+})
 
-initApp();
+
+
+gameOne()
+randomStart()
